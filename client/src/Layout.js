d0bb5fdf619a186555095dc15fd8cdd70be6d0eb
Priyanshu Pandey <2040020@sliet.ac.in>
2023-06-25 21:23:41 +0530

    done

---
+++ client/src/Layout.js
@@ -0,0 +1,13 @@
+/** @format */
+
+import Header from './Header';
+import { Outlet } from 'react-router-dom';
+
+export default function Layout() {
+	return (
+		<main>
+			<Header />
+			<Outlet />
+		</main>
+	);
+}
